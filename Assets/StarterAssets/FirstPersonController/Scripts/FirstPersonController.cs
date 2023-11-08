7f784771406f77df5ce9a029caec482a1b0ef244
JTMGaming <porkkanalaatikko2@gmail.com>
2023-11-08 13:32:40 +0200

    Added animations to player
    - Added animations to player
        Added Idle animation
        Added Walk animation
        Added Run animation

--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -55,7 +55,7 @@ namespace StarterAssets
 		private float _cinemachineTargetPitch;
 
 		// player
-		private float _speed;
+		public float _speed;
 		private float _rotationVelocity;
 		private float _verticalVelocity;
 		private float _terminalVelocity = 53.0f;
@@ -112,7 +112,7 @@ namespace StarterAssets
 			// reset our timeouts on start
 			_jumpTimeoutDelta = JumpTimeout;
 			_fallTimeoutDelta = FallTimeout;
-			animator = GetComponent<Animator>();
+			
 		}
 
 		private void Update()
