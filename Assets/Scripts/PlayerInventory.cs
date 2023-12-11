49f158a82ea5c18fd3597b454ae4f45300901a25
JTMGaming <porkkanalaatikko2@gmail.com>
2023-12-11 18:14:31 +0200

    Added finalboss

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    public Enemyspawn enemyspawn;
     public int NumberOfParts { get; private set; }
 
     public void PartsCollected(){
         NumberOfParts++;
         print("Number of parts " + NumberOfParts);
+        if (HasAllParts()) {
+            enemyspawn.FinalWave();
+        }
     }
 
     public bool HasAllParts() {
